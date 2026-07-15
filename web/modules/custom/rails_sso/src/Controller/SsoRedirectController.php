39c501dd810d2787ec902fb3b65bc929f3862575
Jay Heaslip <jheaslip@comcast.net>
2026-07-15 11:46:56 -0400

    add no cache to controller

--- web/modules/custom/rails_sso/src/Controller/SsoRedirectController.php
+++ web/modules/custom/rails_sso/src/Controller/SsoRedirectController.php
@@ -56,9 +56,7 @@ class SsoRedirectController extends ControllerBase {
       'name'  => $current_user->getAccountName(),
     ];
 
-    error_log("Drupal generate - now: " . time() . " iat: $issued_at exp: $expire");
     $jwt = JWT::encode($payload, $secret_key, 'HS256');
-    error_log("Token: " . $jwt);  // Log the full token
     
     // Construct target
     $rails_url = $rails_base_url . '?token=' . urlencode($jwt);
